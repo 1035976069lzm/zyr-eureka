7bb86eca85a0967e6aed40c00d4a893a342c8ac3
liuzunming <1035976069@qq.com>
2019-07-30 16:22:27 +0800

    first commit

---
+++ src/main/java/zyr/learn/eureka/zyreureka/ZyrEurekaApplication.java
@@ -0,0 +1,13 @@
+package zyr.learn.eureka.zyreureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyrEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZyrEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zyr/learn/eureka/zyreureka/ZyrEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package zyr.learn.eureka.zyreureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZyrEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
